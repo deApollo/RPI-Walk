7906fb80936bf9d7b17cb1c1d1f668466b2c1806
Ezra Dowd <ezra.dowd@gmail.com>
2014-06-02 17:54:58 -0400

    Added work on GUI and started on activity for directions

--- app/src/main/java/com/tezra/rpiwalk/app/MainAct.java
+++ app/src/main/java/com/tezra/rpiwalk/app/MainAct.java
@@ -1,20 +1,67 @@
 package com.tezra.rpiwalk.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainAct extends ActionBarActivity {
-    //Testing
+
+    public final static String EXTRA_MSG = "com.tezra.rpiwalk.MSG";
+
+    String [] locations = {"loc1","loc2","loc3"};
+
+    public void generateToast(CharSequence text, int duration){
+        Toast toast = Toast.makeText(getApplicationContext(),text,duration);
+        toast.show();
+    }
+
+    private boolean validateText(String i, String j){
+        boolean f1 = false; boolean f2 = false;
+        for(String s : locations){
+            if(s.equals(i))
+                f1 = true;
+            if(s.equals(j))
+                f2 = true;
+        }
+        return f1 && f2;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AutoCompleteTextView start = (AutoCompleteTextView) findViewById(R.id.start);
+        final AutoCompleteTextView finish = (AutoCompleteTextView) findViewById(R.id.finish);
+        final ArrayAdapter adp = new ArrayAdapter(this,android.R.layout.select_dialog_item,locations);
+
+        start.setThreshold(0);
+        finish.setThreshold(0);
+        start.setAdapter(adp);
+        finish.setAdapter(adp);
     }
 
+    public void findRoute(View view){
+        String from = ((AutoCompleteTextView) findViewById(R.id.start)).getText().toString();
+        String to = ((AutoCompleteTextView) findViewById(R.id.finish)).getText().toString();
+        if(validateText(from, to)){
+            Intent dirInt = new Intent(this, DirectionsAct.class);
+            String [] locs = {from, to};
+            dirInt.putExtra(EXTRA_MSG,locs);
+            startActivity(dirInt);
+        }
+        else
+            generateToast("Please enter a valid location!",Toast.LENGTH_LONG);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,6 +78,7 @@ public class MainAct extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+
             return true;
         }
         return super.onOptionsItemSelected(item);
